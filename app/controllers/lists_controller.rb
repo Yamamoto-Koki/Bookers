f4e68493773e714b34078add4eb91b4287e0a333
Yamamoto-Koki <130442397+Yamamoto-Koki@users.noreply.github.com>
2023-04-20 13:46:03 +0000

    making validation

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,41 @@
+class ListsController < ApplicationController
+  def index
+    @list = List.new
+    @lists=List.all
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+     @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists' => 'lists#index'
+  post 'lists' => 'lists#create'
+  get 'lists/show'
+  get  'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/top' => 'homes#top'
 end
