5e68539a0c0719cad122221d16eb647f01b03847
Yamamoto-Koki <130442397+Yamamoto-Koki@users.noreply.github.com>
2023-04-22 13:07:46 +0000

    remaining RSpec test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,6 +10,7 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
      if @list.save
       redirect_to list_path(@list.id)
+      flash[:notice] = "Booke was successfully created."
     else
       @lists = List.all
       render :index
@@ -28,12 +29,14 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+    flash[:notice] = "Booke was successfully updated."
   end
 
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
     redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+    flash[:notice] = "Booke was successfully destroyed"
   end
 
   private
