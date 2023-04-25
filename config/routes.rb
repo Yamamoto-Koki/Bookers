1a01ef4002456e63bbfef3f80078fef5ea28bf6c
Yamamoto-Koki <130442397+Yamamoto-Koki@users.noreply.github.com>
2023-04-25 07:55:36 +0000

    to do chekck

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   patch 'lists/:id' => 'lists#update', as: 'update_list'
   delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/top' => 'homes#top'
+  root to: 'homes#top'
 end
